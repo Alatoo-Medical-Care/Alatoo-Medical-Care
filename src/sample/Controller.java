5ced87c49ed6211f62ee40369396b83ea8a710df
askhat <askat.shailoobekovich@mail.ru>
2020-12-16 02:34:02 +0600

    Commit.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,5 @@
 package sample;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -15,19 +13,8 @@ public class Controller {
     private URL location;
 
     @FXML
-    private JFXTextField loginUsername;
-
-    @FXML
-    private JFXTextField loginPassword;
-
-    @FXML
-    private JFXButton loginBtn;
-
-    @FXML
     void initialize() {
-        loginBtn.setOnAction(actionEvent ->{
-            System.out.println("Login Clicked!");
-        });
 
     }
 }
+
