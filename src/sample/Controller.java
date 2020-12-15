93ba1e40394609281b2d56571cabeab8b50df71e
askhat <askat.shailoobekovich@mail.ru>
2020-12-16 02:42:52 +0600

    Commit.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,9 @@ package sample;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
@@ -13,8 +16,18 @@ public class Controller {
     private URL location;
 
     @FXML
-    void initialize() {
+    private TextField loginUsername;
 
+    @FXML
+    private PasswordField loginPassword;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    void initialize() {
+       loginBtn.setOnAction(actionEvent -> {
+           System.out.println("Hello");
+       });
     }
 }
-
