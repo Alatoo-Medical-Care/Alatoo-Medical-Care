b9028e5c97f13e75f54412b354c6d2190eca4c74
askhat <askat.shailoobekovich@mail.ru>
2020-12-16 13:56:32 +0600

    Commit.

--- src/sample/Home.java
+++ src/sample/Home.java
@@ -3,6 +3,7 @@ package sample;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Home {
 
@@ -13,7 +14,19 @@ public class Home {
     private URL location;
 
     @FXML
+    private Button historyBtn;
+
+    @FXML
+    private Button addPatientBtn;
+
+    @FXML
+    private Button upcomingBtn;
+
+    @FXML
     void initialize() {
+        assert historyBtn != null : "fx:id=\"historyBtn\" was not injected: check your FXML file 'home.fxml'.";
+        assert addPatientBtn != null : "fx:id=\"addPatientBtn\" was not injected: check your FXML file 'home.fxml'.";
+        assert upcomingBtn != null : "fx:id=\"upcomingBtn\" was not injected: check your FXML file 'home.fxml'.";
 
     }
 }
